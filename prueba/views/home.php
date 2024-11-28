37cbbfed683e1d8228246568d77a7fdc3554379d
Gustavo <tavojiba55@gmail.com>
2024-11-28 13:15:18 -0600

    avanzamos con inicio

--- prueba/views/home.php
+++ prueba/views/home.php
@@ -1,4 +1,88 @@
-<div>
-
-</div>
+<head>    <link rel="stylesheet" href="<?=CSS.'dama.css'?>"></head>
+<section class="main-content">
+    <div class="container">
+        <h1 class="section-title" >Productos de dama</h1>
+        <div class="cards-container">
+            <!-- Card 1 -->
+            <div class="card">
+                <img src="./public/css/img/calvo.jpg" alt="Producto 1" class="card-image">
+                <div class="card-info">
+                    <button class="btn-action">Comprar</button>
+                </div>
+            </div>
+            <!-- Card 2 -->
+            <div class="card">
+                <img src="./public/css/img/calvo.jpg" alt="Producto 2" class="card-image">
+                <div class="card-info">
+                    <p class="product-name">Pulsera de infinito</p>
+                    <p class="product-price">$50.00</p>
+                    <button class="btn-action">Comprar</button>
+                </div>
+            </div>
+            <!-- Card 3 -->
+            <div class="card">
+                <img src="./public/css/img/calvo.jpg" alt="Producto 3" class="card-image">
+                <div class="card-info">
+                    <p class="product-name">Chamarra negra</p>
+                    <p class="product-price">$300.00</p>
+                    <button class="btn-action">Comprar</button>
+                </div>
+            </div>
+            <!-- Card 4 -->
+            <div class="card">
+                <img src="./public/css/img/calvo.jpg" alt="Producto 4" class="card-image">
+                <div class="card-info">
+                    <p class="product-name">Cartera</p>
+                    <p class="product-price">$120.00</p>
+                    <button class="btn-action">Comprar</button>
+                </div>
+            </div>
+            <!-- Card 5 -->
+            <div class="card">
+                <img src="./public/css/img/calvo.jpg" alt="Producto 5" class="card-image">
+                <div class="card-info">
+                    <p class="product-name">Sudadera top</p>
+                    <p class="product-price">$180.00</p>
+                    <button class="btn-action">Comprar</button>
+                </div>
+            </div>
+            <!-- Card 6 -->
+            <div class="card">
+                <img src="./public/css/img/calvo.jpg" alt="Producto 6" class="card-image">
+                <div class="card-info">
+                    <p class="product-name">juego de bolsa de mano</p>
+                    <p class="product-price">$250.00</p>
+                    <button class="btn-action">Comprar</button>
+                </div>
+            </div>
+            <!-- Card 7 -->
+            <div class="card">
+                <img src="./public/css/img/calvo.jpg" alt="Producto 7" class="card-image">
+                <div class="card-info">
+                    <p class="product-name">Conjunto de dama</p>
+                    <p class="product-price">$190.00</p>
+                    <button class="btn-action">Comprar</button>
+                </div>
+            </div>
+            <!-- Card 8 -->
+            <div class="card">
+                <img src="./public/css/img/calvo.jpg" alt="Producto 8" class="card-image">
+                <div class="card-info">
+                    <p class="product-name">Aretes de oro laminado</p>
+                    <p class="product-price">$150.00</p>
+                    <button class="btn-action">Comprar</button>
+                </div>
+            </div>
+            <!-- Card 9 -->
+            <div class="card">
+                <img src="./public/css/img/calvo.jpg" alt="Producto 9" class="card-image">
+                <div class="card-info">
+                    <p class="product-name">Vestido de moda de dama</p>
+                    <p class="product-price">$200.00</p>
+                    <button class="btn-action">Comprar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php require_once("./views/footer.php"); ?>
